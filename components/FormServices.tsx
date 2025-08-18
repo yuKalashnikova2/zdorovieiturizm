f54d39f80a13a0f2c6cf5c6f05802e78a53fdd4a
yulia <yulia@yulias-MacBook-Pro.local>
2025-08-18 16:36:30 +0300

    [#11] FormServices

---
+++ components/FormServices.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Button from "./ui/Button";
+import LabelPrice from "@/components/LabelPrice";
+import useModal from "@/hooks/useModal";
+import styles from "./Form.module.scss";
+
+type FormProps = {
+  backgroundImage: string;
+};
+
+export default function Form({ backgroundImage }: FormProps) {
+  const { openModal } = useModal();
+
+  return (
+    <div
+      className={styles.form__services}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className={styles.form__services__title}>
+        Запишись на бесплатную консультацию до конца осени!
+      </div>
+
+      <span className={`${styles.form__services__subtitle} mt-20`}>
+        мы подберём лучшее решение и ответим на все вопросы!
+      </span>
+
+      <Button
+        onClick={() => openModal("banner")}
+        className={styles.form__services__btn}
+      >
+        Записаться
+      </Button>
+
+      <div className={`${styles.form__services__labels} mobiles`}>
+        <LabelPrice
+          small
+          colorGray
+          whiteShadow
+          withAuto
+          price="0 руб."
+          text="Предоплаты"
+          color="rgba(255, 255, 255, 0.8)"
+        />
+        <LabelPrice
+          small
+          colorGray
+          whiteShadow
+          withAuto
+          price="Онлайн"
+          text="Поддержка"
+          color="rgba(255, 255, 255, 0.8)"
+        />
+        <LabelPrice
+          small
+          colorGray
+          withAuto
+          price="100%"
+          text="Гарантия"
+          color="rgba(255, 255, 255, 0.8)"
+        />
+      </div>
+    </div>
+  );
+}
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -3,16 +3,17 @@
 import React from 'react';
 import styles from './Button.module.scss';
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   small?: boolean;
   children: React.ReactNode;
   onClick?: () => void;
 }
 
-export default function Button({ small, children, onClick }: ButtonProps) {
+export default function Button({ small, children, onClick, className = '', ...props }: ButtonProps) {
   return (
     <button
-      className={`${styles.button} ${small ? styles.button_small : ''}`}
+      className={`${styles.button} ${small ? styles.button_small : ''} ${className}`}
+      {...props}
       onClick={onClick}
     >
       {children}
